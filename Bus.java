96ebdf9e3b4b60953f6dade9b2e8e3ef57c5ebfe
Rabbit <rohanp.xworkz@gmail.com>
2022-10-11 23:16:17 +0530

    Constructors

---
+++ Bus.java
@@ -0,0 +1,15 @@
+package Xworkz;
+
+public class Bus {
+
+	public String no;
+	public String starting;
+	public String destination;
+	
+	public Bus()
+	{
+		System.out.println("The Journey Begins");
+		
+	}
+
+	}
