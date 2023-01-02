00fa462d5fe54deada8ae6bee4db5ed03a40f903
Rabbit <rohanp.xworkz@gmail.com>
2023-01-02 12:24:49 +0530

    Mock

---
+++ MockTest.java
@@ -0,0 +1,46 @@
+1) Abstraction is hiding the implementation and showing the functionality of the object.
+Abstraction can be achieved in 2ways
+1)Abstract Classes.
+2)Interfaces. 
+
+1)Abstract Classes: Abstract key is used to make Abstract Classes.
+2)Interfaces: implements key is used to inherit the properties of the parent class to child class.
+
+Encapsulation: Whereas ,the wrapping up of the data of the Object is called encapsulation.
+Capsule is real time example for encapsulation.Because it contains many medicines inside it.And Java Bean is a example for fully encapsulated class.
+
+
+3)Inheritance : The process of aquiring all the properties from parent class is Called Inheritance.
+There are 5types of Inheritance and one of them is Multiple Inheritance.
+Multiple Inheritance is group of Single,Multilevel and Heirarchical Inheritance.Where Parent properties are acquired by Child class.
+Multiple Inheritance gives rise to ambiguity where the problem occurs while Inheriting the properties of Parent Class.Hence it is not used supported nowadays.
+It can be achieved by using Interfaces.
+
+7)equals() : This method compares the address of an 2 respective objects where (==) compares the values of 2objects.
+
+9)private : It is a keyword where object can be accessed within the class but not outside the class or package.It can be accessed only
+			if getters and setters methods are used.
+  protected : It is a keword used in declaration of fields which cannot be accessed outside class or package.
+  public : The objects can be accessed within class,outside the class and also anywhere outside the package.
+  
+10)Wrapper Class : It is a process of converting Primitive in Non-Primitive objects.There are 2types ;
+1)Autoboxing : It is a process of converting Primitive into Objects.
+2)Unboxing : It is a process of converting Non-primitive to Primitive objects.
+
+
+12)Heap Memory : It is a memory location where all the objects are stored.
+ Stack Memory : It is a location where methods are being stored.
+ 
+
+15)final : It is a keyword which is used to declare variables,methods and classes of an object.It is used where we cannot change the 
+			values of a object.It is fixed and cannot change the implementation of the object.
+static is also keyword which is used in implementation of an methods,variables and class.The main method should must and should contains
+static keyword because the programs starts executing from the main method.static variables can be accessed using class name or reference 
+of an object directly.
+
+8)We can create constatnts in Java by using enum class.If we declare constants in enum we cannot change the values of particular constants
+in any class when we try to call or invoke it.
+
+
+
+
